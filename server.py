9e46647be510dc2ccd99ed63ecff46ae41b9064e
aindriu18 <aindriuinglis@gmail.com>
2023-02-17 11:28:25 +0000

    Added in ability to post to .txt and .csv files

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 # render template allows us to send html files to webpage
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+import csv
 
 # frameworks give us a higher level of abstraction
 # ginja is a templating language for Flask that allows us to dynamically update our pages
@@ -15,11 +16,37 @@ def my_home(page_name):
     # when using render_template, flask automatically looks for a folder called templates. You need to create this.
     return render_template(page_name)
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+    with open('database.csv', newline="", mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
+
+
 # new route for submitting form on contact page
-#GET means broswer wants to send info. POST means browser wants to save info.
+# GET means broswer wants to send info. POST means browser wants to save info.
+# We never call this on the front end. Works in conjunction with contact.html
 @app.route('/submit_form', methods=['POST', 'GET'])
-def login():
-    return render_template('login.html', error=error)
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect("/thankyou.html")
+        except:
+            return "Did not save to database"
+    else:
+        return "Something went wrong. Try again"
 """
 Below is an example of how we would hard code our html pages
 
